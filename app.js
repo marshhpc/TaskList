bbe89751d844feeca833fc02d8e854fbf775fd87
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-08-18 23:15:06 -0700

    Small project complete

---
+++ app.js
@@ -0,0 +1,166 @@
+// Define Ui Vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
+  // Add task event
+  form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
+}
+
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task) {
+    // Create li element
+    const li = document.createElement("li");
+    // Add Class
+    li.className = "collection-item";
+    // Create text node and append to the li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement("a");
+    // Add class
+    link.className = "delete-item secondary-content";
+    // Add the icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add Class
+  li.className = "collection-item";
+  // Create text node and append to the li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add the icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in local storage
+
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear the input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
+
+// Store Task
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+
+      // Remove from ls
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+// Remove from local storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setitem("tasks", JSON.stringify(tasks));
+}
+
+// Clear Tasks
+function clearTasks() {
+  // Slower
+  // taskList.innerHTML = "";
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // Clear from local Storage
+  clearTasksFromLocalStorage();
+}
+
+// Clear tasks from LS
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  //querySelectorAll retur a node list
+  //if we used get element by class would return html collection and would have to convert to an array
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    // no match equal -1, if its not equal != -1, than we want it to show
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
+
+//This project gives insight to the DOM and Local storage
